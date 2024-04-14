4665e3f3af9e05ffe51d6f5a36664146769dc6f5
vill4in <j.adrian.pulido.s@gmail.com>
2024-04-14 13:33:57 -0600

    Before refactor segmented-slider to pyqt5

--- Components/segmented_slider/waveform_slider.py
+++ Components/segmented_slider/waveform_slider.py
@@ -175,10 +175,10 @@ if __name__ == "__main__":
     from PyQt5.QtWidgets import QHBoxLayout, QPushButton
 
     app = QApplication(sys.argv)
-    window = QMainWindow()
+    window = QMainWindow()export QT_QPA_PLATFORM=wayland
 
     n_steps = 50
-    layout = QHBoxLayout()
+    layout = QHBoxLayout()export QT_QPA_PLATFORM=wayland
 
     fixed_size = QSize(500, 300)
     main_widget = QtWidgets.QWidget()
